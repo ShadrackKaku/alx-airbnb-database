e404aefb43384fc6de4f4700ce021717f9e6d1b4
Shadrack Kaku <kabest255@gmail.com>
2025-07-06 02:24:42 +0000

    updated

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,7 +11,7 @@ GROUP BY
     users.id,
     users.full_name;
 
--- 2. Rank properties based on the total number of bookings (most booked property is rank 1)
+-- 2. Rank properties based on the total number of bookings (most booked property is rank 1, using RANK)
 SELECT
     p.id AS property_id,
     p.title,
@@ -26,3 +26,19 @@ GROUP BY
     p.title
 ORDER BY
     booking_rank ASC;
+
+-- 3. Assign a unique row number to each property ordered by total bookings (using ROW_NUMBER)
+SELECT
+    p.id AS property_id,
+    p.title,
+    COUNT(b.id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_row_number
+FROM
+    properties p
+LEFT JOIN
+    bookings b ON p.id = b.property_id
+GROUP BY
+    p.id,
+    p.title
+ORDER BY
+    booking_row_number ASC;
